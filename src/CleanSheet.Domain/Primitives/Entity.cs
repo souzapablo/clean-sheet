130311cdc037057ba00bca348c3567e57c806889
Pablo Souza <pablo.osouza@outlook.com>
2024-05-16 10:14:10 -0300

    feat: create base entity

---
+++ src/CleanSheet.Domain/Primitives/Entity.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanSheet.Domain.Primitives;
+
+public abstract class Entity(Guid id)
+{
+    public Guid Id { get; private set; } = id;
+    public bool IsDeleted { get; private set; }
+
+    protected void Delete() =>
+        IsDeleted = true;
+}
